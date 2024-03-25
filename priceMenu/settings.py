f3d4a1bb09c2e1dca9b35ceb6fc3bddd573871f8
Taiman Saidaliyev <taiman.saidaliev@atikz.com>
2024-03-25 17:05:56 +0500

    updates

--- priceMenu/settings.py
+++ priceMenu/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
