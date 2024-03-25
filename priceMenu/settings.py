94af8b5e573c2a79a6fb25839ad07197cfbed19d
Taiman Saidaliyev <taiman.saidaliev@atikz.com>
2024-03-25 17:07:51 +0500

    updates

--- priceMenu/settings.py
+++ priceMenu/settings.py
@@ -137,9 +137,9 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:5173',
 ]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
