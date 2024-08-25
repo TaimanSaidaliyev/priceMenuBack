ae70072014f42219481f17f9da94f35c5d606804
Taiman Saidaliyev <taimansaidaliev@gmail.com>
2024-08-25 15:46:23 +0500

    updated CSRF_TRUSTED_ORIGINS

--- priceMenu/settings.py
+++ priceMenu/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+CSRF_TRUSTED_ORIGINS = ['https://doc.ezqr.kz']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
